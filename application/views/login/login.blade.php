0f7a5cd9e0d8852ed0f38e4bdcde82fd4910f424
Amman <ammanwaheedmughal@gmail.com>
2013-02-27 17:20:40 +0500

    layouts edit

--- application/views/login/login.blade.php
+++ application/views/login/login.blade.php
@@ -1,14 +1,5 @@
- {{ Form::open('login', 'POST') }}
-
-      {{ Form::token() }}
-
-      {{ Form::text('email', Input::Old('email') , array('id'=>'email', 'class' => 'input-block-level', 'placeholder' => 'Email')) }}
-
-      {{ Form::text('password', Input::Old('password') , array('id'=>'password', 'class' => 'input-block-level', 'placeholder' => 'Password')) }}
-
-      <div class="submit_sect">
-      {{ Form::submit('Sign In', array('class'=>'btn btn-large btn-primary align-right')) }}
-      </div>
-
-    {{ Form::close() }}
+@layout('layouts.default')
+@section('content')
+    
+@endsection
 
